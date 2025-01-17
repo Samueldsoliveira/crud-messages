4f30b30000671d7eba3d8b3e0c5574ae430f87d1
Samueldsoliveira <samuelzinS10@gmail.com>
2025-01-17 14:47:17 -0300

    adds typeORM support with postgreSQL configuration in the main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      database: 'postgres',
+      password: '123456',
+      autoLoadEntities: true, // loads all entities in the entities folder
+      synchronize: true, // automatically creates tables based on the entities (dont use in production)
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
